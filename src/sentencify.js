3cf1cf2bd7c049cf345634fddf8f1d8a7555aa6b
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-07-16 10:57:34 +0200

    Removing console.log()

--- src/sentencify.js
+++ src/sentencify.js
@@ -1,9 +1,6 @@
 function sentencify(words = []) {
 
   if (words.length === 0) return '';
-
-  console.log('Array: ',words)
-  console.log('CharAt:', words[0].charAt(0) )
   words[0] = `${words[0].charAt(0).toUpperCase()}${words[0].slice(1)}`;
   return `${words.join(' ')}.`;
 }
